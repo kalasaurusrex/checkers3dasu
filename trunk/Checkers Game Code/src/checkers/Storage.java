2891fde42db7e247a784ef4f2ce18e785f9bf802
kalamathkat <kalamathkat@d9369d5a-c4b7-11de-9797-47652b86970f>
2009-10-29 22:55:49 +0000

    added some comments and default Player1 and Player2 for testing

--- trunk/Checkers Game Code/src/checkers/Storage.java
+++ trunk/Checkers Game Code/src/checkers/Storage.java
@@ -7,12 +7,20 @@ import java.util.*;
 // </editor-fold> 
 public class Storage
 {
-    private ArrayList users;
+    private ArrayList<User> users;
     private ArrayList games;
 
+    // generic Storage constructor.  for testing purposes, a user list
+    // containing player1 and player 2 is created.
+    // 
     public Storage () 
     {
-
+        ArrayList<User> users = new ArrayList<User>();
+        User player1 = new User("player1");
+        User player2 = new User("player2");
+        users.add(player1);
+        users.add(player2);
+        users = new ArrayList<User>();
     }
 
     public ArrayList getGames ()
@@ -24,10 +32,12 @@ public class Storage
     {
         this.games = val;
     }
-
+    // the getUsers method returns an ArrayList of all users that have been
+    // previously created in the system.  for testing purposes, this method now
+    // returns a list containing two generic Users, player1 & player2.
     public ArrayList getUsers ()
     {
-        return users;
+       return users;
     }
 
     public void setUsers (ArrayList val)
