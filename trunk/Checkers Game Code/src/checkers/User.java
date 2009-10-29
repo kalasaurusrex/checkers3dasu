4cbfc0b1b2d2db3130dce7e29ff6f00d37916d99
kalamathkat <kalamathkat@d9369d5a-c4b7-11de-9797-47652b86970f>
2009-10-29 22:57:08 +0000

    Override toString, set all users to have a default password of "password"

--- trunk/Checkers Game Code/src/checkers/User.java
+++ trunk/Checkers Game Code/src/checkers/User.java
@@ -7,9 +7,14 @@ public class User
     private String password;
     private boolean admin;
 
+    // generic constructor of a new User object.  all users are assigned the
+    // default password of "password" at this point.
+    // password functionality will be added in build 2.
     public User (String name) 
     {
-
+        userName = name;
+        password = "password";
+        admin = false;
     }
 
     public boolean getAdmin ()
@@ -37,6 +42,9 @@ public class User
         return userName;
     }
 
+    public String toString() {
+        return userName;
+    }
     public void setUserName (String val)
     {
         this.userName = val;
