d160ab5992873a2f5fb45f541936953e3ddfb9a1
mike.sawtelle <mike.sawtelle@d9369d5a-c4b7-11de-9797-47652b86970f>
2009-10-29 20:30:52 +0000

    Initial set up.2

---
+++ src/checkers/King.java
@@ -0,0 +1,24 @@
+//----------------------------------------------------------------------
+//FileName: King.java
+//
+//Author: Byron Lunt
+//
+//Description: This class represents a king game piece.
+//----------------------------------------------------------------------
+package checkers;
+
+
+public class King extends Piece
+{
+    //Attributes////////////////////////////////////////////////////////
+
+    //------------------------------------------------------------------
+    //Method: King()
+    //Description: The default constructor for the class
+    //------------------------------------------------------------------
+    public King (boolean visitorTurn)
+    {
+        super(visitorTurn);
+    }
+
+}
