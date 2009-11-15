b2f859076851fb92981f8f69e90faf40955a7efb
clarkcito@hotmail.com <clarkcito@hotmail.com@d9369d5a-c4b7-11de-9797-47652b86970f>
2009-11-15 21:29:22 +0000

    Move class added to repo

---
+++ trunk/Checkers Game Code/src/checkers/Move.java
@@ -0,0 +1,98 @@
+/*
+ * The Move class helps facilitate communication between the Referee and
+ * GameScreen class while processing moves in 3D Checkers
+ *
+ * Author: David Clark
+ */ 
+
+package checkers;
+
+import java.util.ArrayList;
+
+public class Move
+{
+    private ArrayList<Square> jump;
+    private boolean validMove;
+    private boolean moreJumps;
+    private boolean requiredJump;
+    private int gameOver;
+
+    public Move()
+    {
+        jump = new ArrayList();
+        validMove = false;
+        moreJumps = false;
+        requiredJump = false;
+        gameOver = 0;
+    }
+
+    public void clearMove()
+    {
+        jump.clear();
+        validMove = false;
+        moreJumps = false;
+        requiredJump = false;
+    }
+
+    public void addJump(Square s)
+    {
+        jump.add(s);
+    }
+
+     public ArrayList<Square> getJump()
+    {
+        return jump;
+    }
+
+    public void clearJump()
+    {
+        jump.clear();
+    }
+
+    public int jumpSize()
+    {
+        return jump.size();
+    }
+
+    public void setValidMove(boolean valid)
+    {
+        validMove = valid;
+    }
+
+    public boolean isValidMove()
+    {
+        return validMove;
+    }
+
+    public void setMoreJumps(boolean more)
+    {
+        moreJumps = more;
+    }
+
+    public boolean moreJumps()
+    {
+        return moreJumps;
+    }
+
+    public void setRequiredJump(boolean required)
+    {
+        requiredJump = required;
+    }
+
+    public boolean requiredJump()
+    {
+        return requiredJump;
+    }
+
+    public void setGameOver(int over)
+    {
+        gameOver = over;
+    }
+
+    public int getGameOver()
+    {
+        return gameOver;
+    }
+
+
+}
